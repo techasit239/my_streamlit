54d7109d5a4e7bd924448979054c438675223316
techasit239 <dadsc.239@gmail.com>
2025-11-07 01:55:09 +0700

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -27,12 +27,11 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Map of all pickups')
-st.map(data)
 
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 
 st.map(filtered_data)
+
 
